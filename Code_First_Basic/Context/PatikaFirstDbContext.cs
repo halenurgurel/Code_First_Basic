f47aecd053cad6902b19193408c9c4deb4dc32c2
halenurgurel <halenur.gurel@hotmail.com>
2025-07-30 11:10:05 +0300

    Add project files.

---
+++ Code_First_Basic/Context/PatikaFirstDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Code_First_Basic.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Code_First_Basic.Context
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=HALEONUR; Database=PatikaCodeFirstDb1; Trusted_Connection=true;TrustServerCertificate=true");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
